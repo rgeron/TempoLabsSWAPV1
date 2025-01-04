58f5e4ac2a0831dabc8d39e59c767a72ef37e209
NgrimaldiN-007 <nicolas.grimaldi007@gmail.com>
2025-01-04 16:08:12 +0000

    homegrid

--- src/components/dashboard/screens/HomePage.tsx
+++ src/components/dashboard/screens/HomePage.tsx
@@ -1,8 +1,10 @@
 import AllDecks from "@/components/marketplace/AllDecks";
+import CategoryGrid from "@/components/marketplace/CategoryGrid";
 
 const Home = () => {
   return (
-    <div className="flex-1 overflow-auto px-6 pb-6">
+    <div className="flex-1 overflow-auto space-y-8 px-6 pb-6">
+      <CategoryGrid />
       <AllDecks />
     </div>
   );
