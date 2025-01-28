16c9e03f8a7b555a1dd302fc62ae6e2c75a93108
rgeron <raphaelgeron123@gmail.com>
2025-01-28 13:38:47 +0100

    hummm

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
 export const CLIENT_URL =
   import.meta.env.VITE_CLIENT_URL || "http://localhost:5173";
+
 export const SERVER_URL =
   import.meta.env.VITE_SERVER_URL || "http://localhost:5001";
+
 export const STRIPE_API_URL = `${SERVER_URL}/api`;
