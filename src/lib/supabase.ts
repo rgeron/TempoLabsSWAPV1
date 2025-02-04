5f3b886fc51ab274f97de5e16c9eb20769f93f11
ptiraph <raphaelgeron@icloud.com>
2025-02-04 14:51:54 +0000

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,3 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mrblpcwbxeeqcueoyusb.supabase.co";
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
