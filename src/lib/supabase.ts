fbd57f6756508fc60362753b45264288a85a2ea4
rgeron <raphaelgeron123@gmail.com>
2025-02-04 16:02:30 +0100

    ok

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://mrblpcwbxeeqcueoyusb.supabase.co";
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
