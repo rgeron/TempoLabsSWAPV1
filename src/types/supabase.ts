bf9c0b437d752f0a04c0afa79a6c70f23fb68795
rgeron <raphaelgeron123@gmail.com>
2025-02-05 14:40:12 +0100

    isseller

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,174 +4,177 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       decks: {
         Row: {
-          average_rating: number | null
-          cardcount: number | null
-          categories: string[] | null
-          cover_image_url: string | null
-          created_at: string | null
-          creator_avatar: string | null
-          creatorid: string | null
-          description: string | null
-          difficulty: string | null
-          flashcards_file_url: string | null
-          id: string
-          price: number | null
-          purchase_history: Json | null
-          title: string
-          total_reviews: number | null
-          updated_at: string | null
-        }
+          average_rating: number | null;
+          cardcount: number | null;
+          categories: string[] | null;
+          cover_image_url: string | null;
+          created_at: string | null;
+          creator_avatar: string | null;
+          creatorid: string | null;
+          description: string | null;
+          difficulty: string | null;
+          flashcards_file_url: string | null;
+          id: string;
+          price: number | null;
+          purchase_history: Json | null;
+          title: string;
+          total_reviews: number | null;
+          updated_at: string | null;
+        };
         Insert: {
-          average_rating?: number | null
-          cardcount?: number | null
-          categories?: string[] | null
-          cover_image_url?: string | null
-          created_at?: string | null
-          creator_avatar?: string | null
-          creatorid?: string | null
-          description?: string | null
-          difficulty?: string | null
-          flashcards_file_url?: string | null
-          id: string
-          price?: number | null
-          purchase_history?: Json | null
-          title: string
-          total_reviews?: number | null
-          updated_at?: string | null
-        }
+          average_rating?: number | null;
+          cardcount?: number | null;
+          categories?: string[] | null;
+          cover_image_url?: string | null;
+          created_at?: string | null;
+          creator_avatar?: string | null;
+          creatorid?: string | null;
+          description?: string | null;
+          difficulty?: string | null;
+          flashcards_file_url?: string | null;
+          id: string;
+          price?: number | null;
+          purchase_history?: Json | null;
+          title: string;
+          total_reviews?: number | null;
+          updated_at?: string | null;
+        };
         Update: {
-          average_rating?: number | null
-          cardcount?: number | null
-          categories?: string[] | null
-          cover_image_url?: string | null
-          created_at?: string | null
-          creator_avatar?: string | null
-          creatorid?: string | null
-          description?: string | null
-          difficulty?: string | null
-          flashcards_file_url?: string | null
-          id?: string
-          price?: number | null
-          purchase_history?: Json | null
-          title?: string
-          total_reviews?: number | null
-          updated_at?: string | null
-        }
+          average_rating?: number | null;
+          cardcount?: number | null;
+          categories?: string[] | null;
+          cover_image_url?: string | null;
+          created_at?: string | null;
+          creator_avatar?: string | null;
+          creatorid?: string | null;
+          description?: string | null;
+          difficulty?: string | null;
+          flashcards_file_url?: string | null;
+          id?: string;
+          price?: number | null;
+          purchase_history?: Json | null;
+          title?: string;
+          total_reviews?: number | null;
+          updated_at?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "decks_creatorid_fkey"
-            columns: ["creatorid"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: "decks_creatorid_fkey";
+            columns: ["creatorid"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
       profiles: {
         Row: {
-          avatar_url: string | null
-          created_at: string | null
-          followedcreators: string[] | null
-          id: string
-          likeddeckids: string[] | null
-          purchaseddeckids: string[] | null
-          purchaseinfo: Json[] | null
-          updated_at: string | null
-          username: string
-        }
+          avatar_url: string | null;
+          created_at: string | null;
+          followedcreators: string[] | null;
+          id: string;
+          likeddeckids: string[] | null;
+          purchaseddeckids: string[] | null;
+          purchaseinfo: Json[] | null;
+          updated_at: string | null;
+          username: string;
+          isseller: boolean | null;
+        };
         Insert: {
-          avatar_url?: string | null
-          created_at?: string | null
-          followedcreators?: string[] | null
-          id: string
-          likeddeckids?: string[] | null
-          purchaseddeckids?: string[] | null
-          purchaseinfo?: Json[] | null
-          updated_at?: string | null
-          username: string
-        }
+          avatar_url?: string | null;
+          created_at?: string | null;
+          followedcreators?: string[] | null;
+          id: string;
+          likeddeckids?: string[] | null;
+          purchaseddeckids?: string[] | null;
+          purchaseinfo?: Json[] | null;
+          updated_at?: string | null;
+          username: string;
+          isseller: boolean | null;
+        };
         Update: {
-          avatar_url?: string | null
-          created_at?: string | null
-          followedcreators?: string[] | null
-          id?: string
-          likeddeckids?: string[] | null
-          purchaseddeckids?: string[] | null
-          purchaseinfo?: Json[] | null
-          updated_at?: string | null
-          username?: string
-        }
-        Relationships: []
-      }
+          avatar_url?: string | null;
+          created_at?: string | null;
+          followedcreators?: string[] | null;
+          id?: string;
+          likeddeckids?: string[] | null;
+          purchaseddeckids?: string[] | null;
+          purchaseinfo?: Json[] | null;
+          updated_at?: string | null;
+          username?: string;
+          isseller: boolean | null;
+        };
+        Relationships: [];
+      };
       reviews: {
         Row: {
-          comment: string | null
-          created_at: string | null
-          deck_id: string | null
-          id: string
-          rating: number | null
-          updated_at: string | null
-          user_id: string | null
-        }
+          comment: string | null;
+          created_at: string | null;
+          deck_id: string | null;
+          id: string;
+          rating: number | null;
+          updated_at: string | null;
+          user_id: string | null;
+        };
         Insert: {
-          comment?: string | null
-          created_at?: string | null
-          deck_id?: string | null
-          id: string
-          rating?: number | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          comment?: string | null;
+          created_at?: string | null;
+          deck_id?: string | null;
+          id: string;
+          rating?: number | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Update: {
-          comment?: string | null
-          created_at?: string | null
-          deck_id?: string | null
-          id?: string
-          rating?: number | null
-          updated_at?: string | null
-          user_id?: string | null
-        }
+          comment?: string | null;
+          created_at?: string | null;
+          deck_id?: string | null;
+          id?: string;
+          rating?: number | null;
+          updated_at?: string | null;
+          user_id?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "reviews_deck_id_fkey"
-            columns: ["deck_id"]
-            isOneToOne: false
-            referencedRelation: "decks"
-            referencedColumns: ["id"]
+            foreignKeyName: "reviews_deck_id_fkey";
+            columns: ["deck_id"];
+            isOneToOne: false;
+            referencedRelation: "decks";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "reviews_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "profiles"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-    }
+            foreignKeyName: "reviews_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -180,23 +183,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+      PublicSchema["Views"])
+  ? (PublicSchema["Tables"] &
+      PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -204,20 +207,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -225,20 +228,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -246,24 +249,24 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+  : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+    : never = never
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : never;
